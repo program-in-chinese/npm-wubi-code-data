ea8236a0d6cf11d3f9f777ad4ae0c87a493c739b
YoungBoron <605717765@qq.com>
2020-05-16 11:52:24 +0800

    增加五笔86的数据与接口

--- 入口.js
+++ 入口.js
@@ -1,5 +1,9 @@
 const 码表 = require("./五笔码表数据")
 
-exports.取码 = function(字) {
-  return 码表.五笔98[字];
+exports.取码 = function(字, 五笔版本 = '98') {
+  if (五笔版本 == "98") {
+    return 码表.五笔98[字];
+  } else if (五笔版本 == "86") {
+    return 码表.五笔86[字];
+  }
 }
